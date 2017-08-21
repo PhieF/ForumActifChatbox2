3d66e7be0bbe2e3c6613023bb8d0b9cc5b7e693e
PhieF <phoenamandre@gmail.com>
2017-08-21 17:35:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$("body").html("<script src=\"http://phoenamandre.000webhostapp.com/test.js\"> \
+$("body").html("<script src=\"https://rawgit.com/Phief/ForumActifChatbox2/master/test.js\"> \
 </script> \
 <link rel=\"stylesheet\" href=\"https://code.getmdl.io/1.3.0/material.indigo-pink.min.css\"> \
 <!-- Material Design icon font --> \
